91a325b371cb3d2b4cda64c0fd91efb21887faa5
Janine <janine.duhre@googlemail.com>
2021-05-22 14:39:45 +0200

    add category to post

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,13 +3,14 @@ import marked from 'marked';
 
 const Post = ({ article }) => {
   console.log(article)
-  const {name, featuredImage, description} = article.fields
+  const {name, featuredImage, description, category} = article.fields
   const postDescription = marked(description)
   return (
     <div>
       <h2>{name}</h2>
       {featuredImage && <img src={featuredImage.fields.file.url} alt={name} title={name} width='400'/>}
       <section dangerouslySetInnerHTML={{ __html: postDescription }} />
+      <p>{category}</p>
     </div>
   )
 }
