c14901247b2576b8d9a91997ac788c39fd6c0f3b
Janine <janine.duhre@googlemail.com>
2021-05-29 15:21:36 +0200

    installing react-router-dom, adding About page and Switch-Route in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 import { client } from "./client";
 import Posts from "./components/Posts";
+import About from "./components/About"
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class App extends React.Component {
   state = {
@@ -22,19 +24,28 @@ class App extends React.Component {
 
   render() {
     return (
+      <Router>
       <div className="App">
         <header>
           <h1>Cookbook</h1>
         </header>
         <main>
+        <Switch>
+        <Route exact path="/">
           <div>
             <Posts posts={this.state.articles} />
           </div>
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        </Switch>
         </main>
         <footer>
           <p>© 2021 Cookbook 2.0</p>
         </footer>
       </div>
+      </Router>
     );
   }
 }
